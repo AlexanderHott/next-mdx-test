ef23ec1b230c99e6427a5cd0e3ded6067b833cd4
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2024-04-16 17:56:54 -0400

    add mdx and remark-expressive-code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMdx from "@next/mdx";
+import rehypeRaw from "rehype-raw";
+import remarkExpressiveCode from "remark-expressive-code";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+};
+
+const withMdx = createMdx({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [
+      // The nested array structure is required to pass options
+      // to a remark plugin
+      remarkExpressiveCode,
+    ],
+    rehypePlugins: [rehypeRaw],
+  },
+});
+
+export default withMdx(nextConfig);
